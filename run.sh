bbdbc0c702516e5517b48c73f48ace22395f2c1f
Umesh Parulekar <ucparule@us.ibm.com>
2018-09-24 16:47:21 -0500

    initial commit

---
+++ run.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# 
+# Environment variables
+#
+
+# This is the name of the application that shows up on the Dashboard
+export LOGICAL_APP_NAME="Weather Application"
+
+# This value is prefixed to the build number
+export BUILD_PREFIX=master
+
+# The id of the toolchain that shows up on the brower URL
+export TOOLCHAIN_ID="dca6e715-29ed-4400-b55d-f48acb404540"
+
+# A unique number identifying the build
+export BUILD_NUMBER=1
+
+# API Key of the user
+export IBM_CLOUD_API_KEY="3uUSlrZwWKOnZLxeDPhhPk_05jsARuCOooNwyvwfCuwJ"
+
+# Set the value to true to see detail CLI logs
+#export IDRA_DEBUG=false
+
+
+#
+# Upload a build record for this build, It is assumed that the build was successful
+idra --publishbuildrecord  --branch=master --repositoryurl=https://test-iam-1 --commitid=12344234 --status=pass
+
+#
+# Upload unittest test record for the build
+idra --publishtestresult --filelocation=mocha_pass.json --type=unittest
+
+#
+# Upload CodeCoverge test record for the build
+idra --publishtestresult --filelocation=istanbul_pass.json --type=code
+
+#
+# Invoke a DevOps Insights gate to evaluated a policy based on uploaded data
+idra --evaluategate  --policy=FirstPolicy
+
+#
+# Upload a deployment record, It is assumed that the deployment was successful
+idra --publishdeployrecord  --env=dev --status=pass
